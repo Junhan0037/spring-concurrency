82a9e26a7dbc0842b414c1845e2b2c64aeaad68b
Junhan0037 <wnsgks0037@naver.com>
2022-08-19 20:04:04 +0900

    feat: Race Condition 문제점

--- src/test/java/com/concurrency/domain/stock/service/StockServiceTest.java
+++ src/test/java/com/concurrency/domain/stock/service/StockServiceTest.java
@@ -1,14 +1,19 @@
 package com.concurrency.domain.stock.service;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import com.concurrency.domain.stock.Stock;
 import com.concurrency.domain.stock.repository.StockRepository;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.stream.IntStream;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class StockServiceTest {
@@ -32,6 +37,31 @@ class StockServiceTest {
         stockService.decrease(1L, 1L);
         Stock stock = stockRepository.findById(1L).orElseThrow();
         assertEquals(99, stock.getQuantity());
+    }
+
+    @DisplayName("동시 요청 테스트")
+    @Test
+    public void concurrency() throws InterruptedException {
+        int threadCount = 100;
+        ExecutorService executorService = Executors.newFixedThreadPool(32);
+        CountDownLatch latch = new CountDownLatch(threadCount);
+
+        IntStream.range(0, threadCount).forEach(i -> {
+            executorService.submit(() -> {
+                try {
+                    stockService.decrease(1L, 1L);
+                } finally {
+                    latch.countDown();
+                }
+            });
+        });
+
+        latch.await();
+
+        Stock stock = stockRepository.findById(1L).orElseThrow();
+
+        // 100 - (1 * 100) = 0
+        assertEquals(0L, stock.getQuantity());
     }
 
 }
